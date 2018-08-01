202ee6f45296fb724a60d207778a76a921aef777
suhailali <suhail.ali@sourcebits.com>
2018-08-01 13:01:54 +0530

    code clean

--- app/src/main/java/com/sourcebits/people/bottomnavigationsample/MainActivity.kt
+++ app/src/main/java/com/sourcebits/people/bottomnavigationsample/MainActivity.kt
@@ -27,16 +27,6 @@ class MainActivity : AppCompatActivity() {
                 b.putInt("position", position);
                 fragment.arguments = b;
                 return fragment
- //               when(position){
-//
-//                    0-> return fragment
-//                    1-> return FeaturedFragment.newInstance("Featured")
-//                    2-> return ShopFragment.newInstance("Shop")
-//                    3-> return LoveFragment.newInstance("Love")
-//                    4-> return BagFragment.newInstance("Bag")
- //               }
- //              return LiveFragment.newInstance("Default")
-
             }
 
         }
@@ -50,24 +40,6 @@ class MainActivity : AppCompatActivity() {
         bottomNavigation.setOnNavigationItemReselectedListener(mOnNavigationReselectionListener)
 
 
-    }
-
-    public fun openFragment(fragment: Fragment){
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.container, fragment)
-        //transaction.addToBackStack(null)
-        transaction.commit()
-    }
-
-    public fun openFragmentAddToStack(fragment: Fragment, tag: String){
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.container, fragment)
-        transaction.addToBackStack(tag)
-        transaction.commit()
-    }
-
-    fun showToast(msg:String){
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
 
     internal fun getNavPositionFromMenuItem(menuItem: MenuItem): Int {
@@ -102,86 +74,4 @@ class MainActivity : AppCompatActivity() {
         super.onRestoreInstanceState(savedInstanceState)
         onCreate(savedInstanceState)
     }
-
-
-
-
-   /* private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when(item.itemId) {
-            R.id.navigation_live ->{
-                toolbar.title = getString(R.string.navigation_live)
-                val liveFragment = LiveFragment.newInstance(toolbar.title.toString())
-                openFragment(liveFragment)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_featured->{
-                toolbar.title = getString(R.string.navigation_featured)
-                val liveFragment = FeaturedFragment.newInstance(toolbar.title.toString())
-                openFragment(liveFragment)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_shop ->{
-                toolbar.title = getString(R.string.navigation_shop)
-                val liveFragment = ShopFragment.newInstance(toolbar.title.toString())
-                openFragment(liveFragment)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_love ->{
-                toolbar.title = getString(R.string.navigation_love)
-                val liveFragment = LoveFragment.newInstance(toolbar.title.toString())
-                openFragment(liveFragment)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_bag ->{
-                toolbar.title = getString(R.string.navigation_bag)
-                val liveFragment = BagFragment.newInstance(toolbar.title.toString())
-                openFragment(liveFragment)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
-
-
-    private val mOnNavigationReselectionListener = BottomNavigationView.OnNavigationItemReselectedListener{ item ->
-
-        when(item.itemId) {
-            R.id.navigation_live ->{
-                toolbar.title = getString(R.string.navigation_live)
-//                val liveFragment = LiveFragment.newInstance(toolbar.title.toString())
-//                openFragment(liveFragment)
-                showToast(toolbar.title.toString() +  " reselected")
-                return@OnNavigationItemReselectedListener
-            }
-            R.id.navigation_featured->{
-                toolbar.title = getString(R.string.navigation_featured)
-//                val liveFragment = FeaturedFragment.newInstance(toolbar.title.toString())
-//                openFragment(liveFragment)
-                showToast(toolbar.title.toString() +  " reselected")
-                return@OnNavigationItemReselectedListener
-            }
-            R.id.navigation_shop ->{
-                toolbar.title = getString(R.string.navigation_shop)
-//                val liveFragment = ShopFragment.newInstance(toolbar.title.toString())
-//                openFragment(liveFragment)
-                showToast(toolbar.title.toString() +  " reselected")
-                return@OnNavigationItemReselectedListener
-            }
-            R.id.navigation_love ->{
-                toolbar.title = getString(R.string.navigation_love)
-//                val liveFragment = LoveFragment.newInstance(toolbar.title.toString())
-//                openFragment(liveFragment)
-                showToast(toolbar.title.toString() +  " reselected")
-                return@OnNavigationItemReselectedListener
-            }
-            R.id.navigation_bag ->{
-                toolbar.title = getString(R.string.navigation_bag)
-//                val liveFragment = BagFragment.newInstance(toolbar.title.toString())
-//                openFragment(liveFragment)
-                showToast(toolbar.title.toString() +  " reselected")
-                return@OnNavigationItemReselectedListener
-            }
-        }
-    }*/
-
 }
